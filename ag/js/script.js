2c96a88d060824fb9b9add2bc4d2c3de0e0dc612
Marta Jorgacevic <martakord@gmail.com>
2016-08-01 09:32:16 +0200

    moving functions from html to js

--- ag/js/script.js
+++ ag/js/script.js
@@ -8,8 +8,32 @@ function messageInfoHeight() {
     message.css("height", div/3+5);
 }
 
+function datePicker() {
+	$("#date-from, #date-to").datepicker();
+}
+
+function colorChange() {
+	$("#cp-name, #cp-description, .timing input").on("input", function() {
+		$("#cp-save").css("background", "#63d0ec");
+	});
+
+	$("#edit-tab .timing input").on("change", function() {
+		$("#cp-save").css("background", "#63d0ec");
+	});
+
+	$("#cp-name").on("input", function() {
+		$("#cp-create").css("background", "#63d0ec");
+
+		if(!$("#cp-name").val()) {
+			$("#cp-create").css("background", "#989798");
+		}
+	});
+}
+
 $(document).ready(function() {
    messageInfoHeight();
+   datePicker();
+   colorChange();
 });
 
 $(window).resize(function() {
