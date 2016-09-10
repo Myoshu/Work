f4bfd5eb051f7708586f7711ac4a01e4257f399c
Marta Jorgacevic <martakord@gmail.com>
2016-09-10 16:01:27 +0200

    loader added

--- Colourlab/js/script.js
+++ Colourlab/js/script.js
@@ -2,6 +2,10 @@ $("#backtotop").click(function () {
    $("html, body").animate({scrollTop: 0}, 1000);
 });
 
+$(window).on('load', function(){
+	$('#h-loader').fadeOut(4000);
+});
+
 function showImages(el) {
     var windowHeight = $(window).height();
     $(el).each(function(){
