04c2128f4c9f740bb57ff99f12b68f6facc18573
Marta Jorgacevic <martakord@gmail.com>
2016-09-10 07:11:24 +0200

    responsive

--- Colourlab/js/script.js
+++ Colourlab/js/script.js
@@ -1,3 +1,31 @@
 $("#backtotop").click(function () {
    $("html, body").animate({scrollTop: 0}, 1000);
 });
+
+if($(body).length){ (function(){
+
+	$('.feel-slider').mousemove(function(e){
+		var pos = e.pageX / $(window).width() * 100 + '%';
+		$('.feel-slider .image-2').css('width', pos);
+		$('.feel-slider .slidebar').css('left', pos);
+	});
+
+	$('.feel-slider').mouseleave(function(e){
+		$('.feel-slider .image-2').css('width', '60%');
+		$('.feel-slider .slidebar').css('left', '60%');
+	});
+
+	$('.feel-switcher a').click(function(e){
+		var id = $(this).attr('id').substring(7);
+
+		$('.feel-switcher a').removeClass('active');
+		for (var i = id; i >= 0; i--) {
+			$('.feel-switcher a:nth-child(' + i + ')').addClass('active');
+		}
+
+		$('.feel-slider .feels:visible').fadeOut(200, function() { $('.feel-slider #feel-' + id).fadeIn(200); });
+
+		return false;
+	});
+
+})();}
