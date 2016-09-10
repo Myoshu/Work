2436a9e203e6e6716a5a6d99c35755d0a75822aa
Marta Jorgacevic <martakord@gmail.com>
2016-09-10 07:46:38 +0200

    fadein animation

--- Colourlab/js/script.js
+++ Colourlab/js/script.js
@@ -2,30 +2,22 @@ $("#backtotop").click(function () {
    $("html, body").animate({scrollTop: 0}, 1000);
 });
 
-if($(body).length){ (function(){
+function showImages(el) {
+    var windowHeight = $(window).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
 
-	$('.feel-slider').mousemove(function(e){
-		var pos = e.pageX / $(window).width() * 100 + '%';
-		$('.feel-slider .image-2').css('width', pos);
-		$('.feel-slider .slidebar').css('left', pos);
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 100 > thisPos) {
+            $(this).addClass("fadeIn");
+        }
+    });
+}
 
-	$('.feel-slider').mouseleave(function(e){
-		$('.feel-slider .image-2').css('width', '60%');
-		$('.feel-slider .slidebar').css('left', '60%');
-	});
+$(document).ready(function(){
+    showImages('.load');
+});
 
-	$('.feel-switcher a').click(function(e){
-		var id = $(this).attr('id').substring(7);
-
-		$('.feel-switcher a').removeClass('active');
-		for (var i = id; i >= 0; i--) {
-			$('.feel-switcher a:nth-child(' + i + ')').addClass('active');
-		}
-
-		$('.feel-slider .feels:visible').fadeOut(200, function() { $('.feel-slider #feel-' + id).fadeIn(200); });
-
-		return false;
-	});
-
-})();}
+$(window).scroll(function() {
+    showImages('.load');
+});
